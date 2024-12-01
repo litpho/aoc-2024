6e28b7ffb0e620974435ec4f9d7f227903d01b9d
Jasper de Vries <jasper.devries@the-future-group.com>
2024-12-01 23:51:42 +0100

    Cleanup day 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -5,25 +5,29 @@ const DATA: &str = include_str!("input.txt");
 fn main() {
     let (took, result) = took::took(|| parse(DATA));
     println!("Time spent parsing: {took}");
-    let mut input = result;
+    let input = result;
 
-    let (took, result) = took::took(|| part_one(&mut input.0, &mut input.1));
+    let (took, result) = took::took(|| part_one(input.0, input.1));
     println!("Result part one: {result}");
     println!("Time spent: {took}");
 
-    let (took, result) = took::took(|| part_two(&input.0, input.1));
+    let (took, result) = took::took(|| parse(DATA));
+    println!("Time spent parsing: {took}");
+    let input = result;
+
+    let (took, result) = took::took(|| part_two(input.0, input.1));
     println!("Result part two: {result}");
     println!("Time spent: {took}");
 }
 
-fn part_one(left: &mut [usize], right: &mut [usize]) -> usize {
+fn part_one(mut left: Vec<usize>, mut right: Vec<usize>) -> usize {
     left.sort();
     right.sort();
 
     (0..left.len()).map(|i| left[i].abs_diff(right[i])).sum()
 }
 
-fn part_two(left: &[usize], right: Vec<usize>) -> usize {
+fn part_two(left: Vec<usize>, right: Vec<usize>) -> usize {
     let map = right.iter().counts_by(|u| u);
 
     left.iter().map(|l| l * map.get(l).unwrap_or(&0)).sum()
@@ -50,25 +54,25 @@ mod tests {
 
     #[test]
     fn test_part_one_testdata() {
-        let mut input = parse(TESTDATA);
-        assert_eq!(11, part_one(&mut input.0, &mut input.1));
+        let input = parse(TESTDATA);
+        assert_eq!(11, part_one(input.0, input.1));
     }
 
     #[test]
     fn test_part_one() {
-        let mut input = parse(DATA);
-        assert_eq!(2742123, part_one(&mut input.0, &mut input.1));
+        let input = parse(DATA);
+        assert_eq!(2742123, part_one(input.0, input.1));
     }
 
     #[test]
     fn test_part_two_testdata() {
         let input = parse(TESTDATA);
-        assert_eq!(31, part_two(&input.0, input.1));
+        assert_eq!(31, part_two(input.0, input.1));
     }
 
     #[test]
     fn test_part_two() {
         let input = parse(DATA);
-        assert_eq!(21328497, part_two(&input.0, input.1));
+        assert_eq!(21328497, part_two(input.0, input.1));
     }
 }
