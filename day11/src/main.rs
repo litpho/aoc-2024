3d32f6479bf46e6c7195dab6fd65870e63d40b85
Jasper de Vries <jasper.devries@the-future-group.com>
2024-12-11 11:00:18 +0100

    Day 11

---
+++ day11/src/main.rs
@@ -0,0 +1,99 @@
+use itertools::Itertools;
+use std::collections::HashMap;
+
+const DATA: &str = include_str!("input.txt");
+
+fn main() {
+    let (took, result) = took::took(|| parse_input(DATA));
+    println!("Time spent parsing: {took}");
+    let input = result;
+
+    let (took, result) = took::took(|| part_one(input));
+    println!("Result part one: {result}");
+    println!("Time spent: {took}");
+
+    let (took, result) = took::took(|| parse_input(DATA));
+    println!("Time spent parsing: {took}");
+    let input = result;
+
+    let (took, result) = took::took(|| part_two(input));
+    println!("Result part two: {result}");
+    println!("Time spent: {took}");
+}
+
+fn part_one(input: HashMap<usize, usize>) -> usize {
+    solve(input, 25)
+}
+
+fn part_two(input: HashMap<usize, usize>) -> usize {
+    solve(input, 75)
+}
+
+fn solve(input: HashMap<usize, usize>, count: usize) -> usize {
+    (0..count).fold(input, |map, _| blink(&map)).values().sum()
+}
+
+fn blink(input: &HashMap<usize, usize>) -> HashMap<usize, usize> {
+    let mut result = HashMap::new();
+    for (key, count) in input {
+        if key == &0 {
+            result
+                .entry(1)
+                .and_modify(|x| *x += count)
+                .or_insert(*count);
+            continue;
+        }
+
+        let length = key.ilog10() + 1;
+        if length % 2 == 0 {
+            let exp = 10usize.pow(length / 2);
+            let left = key / exp;
+            let right = key % exp;
+            result
+                .entry(left)
+                .and_modify(|x| *x += count)
+                .or_insert(*count);
+            result
+                .entry(right)
+                .and_modify(|x| *x += count)
+                .or_insert(*count);
+            continue;
+        }
+
+        result
+            .entry(key * 2024)
+            .and_modify(|x| *x += count)
+            .or_insert(*count);
+    }
+
+    result
+}
+
+fn parse_input(input: &'static str) -> HashMap<usize, usize> {
+    input
+        .split_whitespace()
+        .map(|c| c.parse::<usize>().unwrap())
+        .counts()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TESTDATA: &str = include_str!("test.txt");
+
+    #[test]
+    fn test_part_one_testdata() {
+        assert_eq!(55312, part_one(parse_input(TESTDATA)));
+    }
+
+    #[test]
+    fn test_part_one() {
+        assert_eq!(224529, part_one(parse_input(DATA)));
+    }
+
+    #[test]
+    fn test_part_two() {
+        assert_eq!(266820198587914, part_two(parse_input(DATA)));
+    }
+}
